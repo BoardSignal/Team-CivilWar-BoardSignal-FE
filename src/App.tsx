c73affa13dedbc3d3668ef5ecb5867fcf0802daa
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-02-08 16:31:59 +0900

    chore: esLint prettier 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './Router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient({
   defaultOptions: {
