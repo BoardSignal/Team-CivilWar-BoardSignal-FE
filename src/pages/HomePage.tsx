d886c9798c175de23f2a5638a25a626a879235ec
Seongbin Kim <seongbin9786@gmail.com>
2024-02-08 19:47:46 +0900

    feat: 참고용 Route 추가, Page에 GNB 배치

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,12 @@
+import { GNB } from '@/components/gnb';
+
 export const HomePage = () => {
-  return <div>Hello</div>;
+  return (
+    <div className='flex h-screen flex-col'>
+      <div className='grow'>
+        <h1>HomePage</h1>
+      </div>
+      <GNB />
+    </div>
+  );
 };
