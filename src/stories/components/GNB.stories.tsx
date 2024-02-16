cde89c1cb30fd278f98849ee3487988fec1a5667
Seongbin Kim <seongbin9786@gmail.com>
2024-02-16 20:46:25 +0900

    fix: Storybook에서 Story의 라우팅을 /에서 *로 변경해 GNB 클릭 시에도 안전하게

--- src/stories/components/GNB.stories.tsx
+++ src/stories/components/GNB.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { reactRouterParameters } from 'storybook-addon-react-router-v6';
 
 import { GNB } from '@/components/GNB';
 
@@ -7,13 +6,6 @@ const meta: Meta<typeof GNB> = {
   title: 'components/GNB',
   tags: ['autodocs'],
   component: GNB,
-  parameters: {
-    reactRouter: reactRouterParameters({
-      routing: {
-        path: '*',
-      },
-    }),
-  },
 };
 
 export default meta;
