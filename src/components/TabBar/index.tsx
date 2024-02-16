5d351393bcd6c709cc7aae9a470c04ce8cbb260b
Seongbin Kim <seongbin9786@gmail.com>
2024-02-16 22:41:21 +0900

    feat: TabBar 구현

---
+++ src/components/TabBar/index.tsx
@@ -0,0 +1,94 @@
+import React, { ComponentPropsWithoutRef, PropsWithChildren } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import Icon from '../Icon';
+
+/**
+ * TabBar를 구성하는 요소에요.
+ *
+ * 간결함과 확장성을 위해서 Compound Pattern으로 개발했어요.
+ *
+ * 자식은 최대 2개까지만 렌더링해요.
+ * 자식으로는 <TabBar.Left>, <TabBar.Right>를 사용해주세요.
+ *
+ * 사용 예시
+ * @example
+ * <TabBar.Container>
+ *   <TabBar.Left>
+ *     <TabBar.GoBackButton />
+ *     <TabBar.Title>안녕하세요</TabBar.Title>
+ *   </TabBar.Left>
+ *   <TabBar.Right>
+ *     <TabBar.ShareButton onClick={...} />
+ *   </TabBar.Right>
+ * </TabBar.Container>
+ */
+const Container = ({ children: rawChildren }: PropsWithChildren) => {
+  const children = React.Children.toArray(rawChildren);
+  const leftChildren = children.slice(0, 1);
+  const rightChildren = children.slice(1, 1);
+
+  return (
+    <div className='flex justify-between p-2'>
+      {leftChildren}
+      {rightChildren}
+    </div>
+  );
+};
+
+const Left = ({ children }: PropsWithChildren) => (
+  <div className='flex items-center gap-1'>{children}</div>
+);
+
+const Right = ({ children }: PropsWithChildren) => (
+  <div className='flex items-center gap-4'>{children}</div>
+);
+
+const Title = ({ children }: PropsWithChildren) => (
+  <div className='cursor-default font-bold'>{children}</div>
+);
+
+type HTMLButtonProps = ComponentPropsWithoutRef<'button'>;
+
+const GoBackButton = ({ onClick, ...props }: HTMLButtonProps) => {
+  const navigate = useNavigate();
+  const goBack = () => navigate(-1);
+
+  return (
+    <button onClick={onClick || goBack} {...props}>
+      <Icon id='arrow-left-line' size={24} />
+    </button>
+  );
+};
+
+const ChatButton = (props: HTMLButtonProps) => (
+  <button {...props}>
+    <Icon id='chat-line' />
+  </button>
+);
+
+const ShareButton = (props: HTMLButtonProps) => (
+  <button {...props}>
+    <Icon id='share-line' />
+  </button>
+);
+
+const SettingsButton = (props: HTMLButtonProps) => (
+  <button {...props}>
+    <Icon id='settings-line' />
+  </button>
+);
+
+const TabBar = {
+  Container,
+  Left,
+  Right,
+  Title,
+  GoBackButton,
+  ChatButton,
+  ShareButton,
+  SettingsButton,
+};
+
+export default TabBar;
