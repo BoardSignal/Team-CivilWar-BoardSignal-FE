376e889ea7b1309bf8459d72e2cbc24067da030e
Seongbin Kim <seongbin9786@gmail.com>
2024-02-16 13:43:10 +0900

    fix: Ripple에 className을 제공해야 하는 경우를 위해 prop 추가

--- src/components/Ripple/index.tsx
+++ src/components/Ripple/index.tsx
@@ -1,5 +1,7 @@
 import React, { PropsWithChildren, useRef, useState } from 'react';
 
+import { cn } from '@/utils/cn';
+
 import RippleEffect from './RippleEffect';
 
 const RIPPLE_DURATION = 500;
@@ -19,7 +21,16 @@ const getOffset = (e: React.MouseEvent<HTMLDivElement>, el: HTMLDivElement) => {
   };
 };
 
-const Ripple = ({ children }: PropsWithChildren) => {
+interface RippleProps extends PropsWithChildren {
+  className?: string;
+}
+
+/**
+ * Ripple 트랜지션을 렌더링하는 컨테이너 컴포넌트에요.
+ *
+ * div로 구현되기 때문에 일부 스타일을 Ripple에 적용해야 할 수 있어요.
+ */
+const Ripple = ({ className, children }: RippleProps) => {
   const ref = useRef<HTMLDivElement>(null);
   const [isAnimating, setIsAnimating] = useState(false);
   const [offset, setOffset] = useState(RIPPLE_DEFAULT_OFFSET);
@@ -40,7 +51,11 @@ const Ripple = ({ children }: PropsWithChildren) => {
   };
 
   return (
-    <div className='relative overflow-hidden' ref={ref} onClick={showRipple}>
+    <div
+      className={cn('relative overflow-hidden', className)}
+      ref={ref}
+      onClick={showRipple}
+    >
       {isAnimating && <RippleEffect {...offset} />}
       {children}
     </div>
