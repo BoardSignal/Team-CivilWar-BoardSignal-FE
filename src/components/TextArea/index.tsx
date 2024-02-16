775a9c2f2333ef30b612aca09654fce8229ebab9
lja0395 <kittyja1023@naver.com>
2024-02-16 11:39:04 +0900

    refactor: 색상 코드 수정

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -12,20 +12,20 @@ const Textarea = ({
 }: TextareaProps) => {
   return (
     <div className='flex flex-col'>
-      <div className='flex items-center justify-between text-gray-700'>
+      <div className='flex items-center justify-between text-gray-accent2'>
         {label && (
           <label>
             {label}
             <span className='ml-0.5 text-red-500'>*</span>
           </label>
         )}
-        <span className='text-sm text-gray-400'>
+        <span className='text-sm text-gray-accent3'>
           {limit && <label>{limit}</label>}/500자
         </span>
       </div>
       <div className='relative'>
         <textarea
-          className='h-[180px] w-full resize-none overscroll-contain rounded-lg border-2 border-gray-200 outline-none focus:border-violet-500'
+          className='h-[180px] w-full resize-none overscroll-contain rounded-lg border border-gray-accent7 outline-none focus:border-primary'
           {...props}
         />
       </div>
