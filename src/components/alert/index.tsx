d77b1b0102ca9b51e4b0865e483818029ae60023
lja0395 <kittyja1023@naver.com>
2024-02-16 14:09:21 +0900

    feat: Alert 공통 컴포넌트 구현

---
+++ src/components/alert/index.tsx
@@ -0,0 +1,24 @@
+interface AlertProps {
+  text?: string;
+  backgroundColor?: string;
+  textColor?: string;
+}
+
+const Alert = ({ text, backgroundColor, textColor }: AlertProps) => {
+  const alertStyle = {
+    backgroundColor,
+    color: textColor,
+  };
+
+  return (
+    <div
+      className='mx-auto my-4 flex w-full rounded-md px-6 py-4'
+      style={alertStyle}
+    >
+      <label className='font-semibold'>안내</label>
+      <span className='ml-2'>{text}</span>
+    </div>
+  );
+};
+
+export default Alert;
---
+++ src/stories/components/Alert.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Alert from '@/components/alert/index';
+
+const meta: Meta<typeof Alert> = {
+  title: 'Components/Textarea',
+  tags: ['autodocs'],
+  component: Alert,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Alert>;
+
+export const Default: Story = {
+  args: {
+    text: '텍스트',
+    backgroundColor: '',
+    textColor: '',
+  },
+};
