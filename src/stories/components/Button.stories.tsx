21ba4ec4ae1aa1868d779f6c05f1fd2d2c4f347e
Seongbin Kim <seongbin9786@gmail.com>
2024-02-18 11:41:06 +0900

    docs: ghost 버튼의 Story 추가

--- src/stories/components/Button.stories.tsx
+++ src/stories/components/Button.stories.tsx
@@ -19,7 +19,15 @@ export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const DefaultTemplate: Story = {
+export const DefaultGhost: Story = {
+  name: '기본 버튼(Ghost)',
+  args: {
+    children: '회원가입 없이 둘러보기',
+  },
+};
+
+export const Primary: Story = {
+  name: 'Primary 버튼',
   args: {
     children: '리뷰 제출하기',
     variant: 'primary',
