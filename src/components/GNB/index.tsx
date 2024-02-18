da49d8bccf2aae5a1b86866df6dcb943f7fbdedc
Seongbin Kim <seongbin9786@gmail.com>
2024-02-18 11:27:25 +0900

    fix: GNB에도 Ripple을 직접 사용하도록 변경

--- src/components/GNB/index.tsx
+++ src/components/GNB/index.tsx
@@ -11,15 +11,13 @@ export const GNB = () => {
     <div className='flex select-none border-t border-gray-200'>
       {GNBRoutes.map(({ name, path, icons }) => (
         <Link to={path} key={path} className='flex-1'>
-          <Ripple fast>
-            <div className='flex flex-col items-center gap-0.5 pt-4'>
-              <Icon
-                id={icons[pathname === path ? 'fill' : 'line']}
-                className='h-5 w-5 text-gray-accent1'
-              />
-              <div className='px-2 pb-2 text-[10px] text-gray-accent1'>
-                {name}
-              </div>
+          <Ripple fast className='flex flex-col items-center gap-0.5 pt-4'>
+            <Icon
+              id={icons[pathname === path ? 'fill' : 'line']}
+              className='h-5 w-5 text-gray-accent1'
+            />
+            <div className='px-2 pb-2 text-[10px] text-gray-accent1'>
+              {name}
             </div>
           </Ripple>
         </Link>
