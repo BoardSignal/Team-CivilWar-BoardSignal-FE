3678ce1a8b281e06a61c31e6482510b801fe8f6b
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-02-19 21:01:25 +0900

    refactor: 라벨 텍스트 사이즈 변경

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -3,7 +3,6 @@ import { PropsWithChildren } from 'react';
 interface LabelProps {
   labelText: string;
   isRequired?: boolean;
-
   isLimitedText?: boolean;
   textValue?: string;
   textLimited?: number;
@@ -16,24 +15,17 @@ const Label = ({
   textValue,
   textLimited,
   isLimitedText = false,
-  width = 'large',
   children,
 }: PropsWithChildren<LabelProps>) => {
-  const widthType = {
-    small: 'max-w-[100px]',
-    medium: 'max-w-[240px]',
-    large: 'max-w-[358px]',
-  };
-
   return (
-    <div className={`flex flex-col ${widthType[width]} gap-2`}>
+    <div className={`flex flex-col gap-2`}>
       <div className='flex justify-between'>
-        <label className='text-gray-accent2'>
+        <label className='text-sm text-gray-accent2'>
           {labelText}
           {isRequired && <span className='ml-0.5 text-red-500'>*</span>}
         </label>
         {isLimitedText && (
-          <span className='text-gray-accent2'>{`${textValue ? textValue.length : 0}/${textLimited}`}</span>
+          <span className='text-sm text-gray-accent2'>{`${textValue ? textValue.length : 0}/${textLimited}`}</span>
         )}
       </div>
       {children}
