77148ca3b5a29caa7ceb69fc16fefbedf11cf322
lja0395 <kittyja1023@naver.com>
2024-02-19 11:12:21 +0900

    refactor: style 수정

--- src/components/alert/index.tsx
+++ src/components/alert/index.tsx
@@ -15,7 +15,7 @@ const Alert = ({ text, backgroundColor, textColor }: AlertProps) => {
       className='mx-auto my-4 flex w-full rounded-md px-6 py-4'
       style={alertStyle}
     >
-      <label className='font-semibold'>안내</label>
+      <label className='font-bold'>안내</label>
       <span className='ml-2'>{text}</span>
     </div>
   );
