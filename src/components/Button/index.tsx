f8a7c68e754212f7be36c46b8b45cf2f1a1ef818
Seongbin Kim <seongbin9786@gmail.com>
2024-02-19 19:43:22 +0900

    [#32] Fix: Button의 Ripple의 사용 방식 변경 및 ghost variant 추가 (#33)

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,33 +1,42 @@
-import { ButtonHTMLAttributes } from 'react';
+import { ComponentProps } from 'react';
 
 import { type VariantProps, cva } from 'class-variance-authority';
 
+import { cn } from '@/utils/cn';
+
 import Ripple from '../Ripple';
 
-const buttonCSS = cva('h-10 w-full rounded-lg', {
-  variants: {
-    variant: {
-      primary: 'bg-primary text-white',
-      outline: 'border border-gray-accent5 text-gray-accent3',
-      danger: 'border border-red-500 text-red-500',
-      inactive: 'cursor-not-allowed bg-gray-accent5 text-gray-accent3',
+const buttonCSS = cva(
+  'flex h-10 w-full cursor-pointer items-center justify-center rounded-lg',
+  {
+    variants: {
+      variant: {
+        primary: 'bg-primary text-white',
+        outline: 'border border-gray-accent5 text-gray-accent3',
+        ghost: 'text-gray-accent2',
+        danger: 'border border-red-500 text-red-500',
+        inactive: 'cursor-not-allowed bg-gray-accent5 text-gray-accent3',
+      },
+    },
+    defaultVariants: {
+      variant: 'ghost',
     },
   },
-});
+);
 
-interface ButtonProps
-  extends ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonCSS> {}
+type ButtonProps = ComponentProps<'div'> & VariantProps<typeof buttonCSS>;
 
-const Button = ({ variant, ...props }: ButtonProps) => {
+const Button = ({ variant, className, ...props }: ButtonProps) => {
   return (
-    <Ripple>
-      <button
-        disabled={variant === 'inactive'}
-        className={buttonCSS({ variant })}
-        {...props}
-      />
-    </Ripple>
+    <Ripple
+      role='button'
+      className={cn(
+        buttonCSS({ variant }),
+        variant === 'inactive' && 'pointer-events-none',
+        className,
+      )}
+      {...props}
+    />
   );
 };
 
--- src/components/GNB/index.tsx
+++ src/components/GNB/index.tsx
@@ -1,7 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 
+import Button from '../Button';
 import Icon from '../Icon';
-import Ripple from '../Ripple';
 import { GNBRoutes } from './routes';
 
 export const GNB = () => {
@@ -11,17 +11,13 @@ export const GNB = () => {
     <div className='flex select-none border-t border-gray-200'>
       {GNBRoutes.map(({ name, path, icons }) => (
         <Link to={path} key={path} className='flex-1'>
-          <Ripple fast>
-            <div className='flex flex-col items-center gap-0.5 pt-4'>
-              <Icon
-                id={icons[pathname === path ? 'fill' : 'line']}
-                className='h-5 w-5 text-gray-accent1'
-              />
-              <div className='px-2 pb-2 text-[10px] text-gray-accent1'>
-                {name}
-              </div>
-            </div>
-          </Ripple>
+          <Button className='flex h-fit flex-col items-center gap-0.5 pb-2 pt-4'>
+            <Icon
+              id={icons[pathname === path ? 'fill' : 'line']}
+              className='h-5 w-5 text-gray-accent1'
+            />
+            <div className='text-[10px] text-gray-accent1'>{name}</div>
+          </Button>
         </Link>
       ))}
     </div>
