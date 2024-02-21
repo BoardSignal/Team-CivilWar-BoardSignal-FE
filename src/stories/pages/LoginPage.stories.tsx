3342c28f982133684c599ef700e99b4916dc67fc
Seongbin Kim <seongbin9786@gmail.com>
2024-02-21 15:46:48 +0900

    fix: Story에 페이지 공통 decorator 반영

--- src/stories/pages/LoginPage.stories.tsx
+++ src/stories/pages/LoginPage.stories.tsx
@@ -2,9 +2,12 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import LoginPage from '@/pages/Login';
 
+import { CommonPageLayoutDecorator } from '../CommonPageLayoutDecorator';
+
 const meta: Meta<typeof LoginPage> = {
   title: 'pages/LoginPage',
   tags: ['autodocs'],
+  decorators: [CommonPageLayoutDecorator],
   component: LoginPage,
 };
 
