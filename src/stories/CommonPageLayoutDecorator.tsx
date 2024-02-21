6ea66b24cd10c61d8e32ad178811e1d263825b7e
Seongbin Kim <seongbin9786@gmail.com>
2024-02-21 14:50:30 +0900

    fix: Decorator Wrapper에 relative 없던 문제 수정

--- src/stories/CommonPageLayoutDecorator.tsx
+++ src/stories/CommonPageLayoutDecorator.tsx
@@ -3,7 +3,10 @@ import { type Decorator } from '@storybook/react';
 import { LAYOUT_ROOT_ID } from '@/components/Layout';
 
 export const CommonPageLayoutDecorator: Decorator = Story => (
-  <div id={LAYOUT_ROOT_ID} className='h-[844px] w-full shrink-0 shadow-xl'>
+  <div
+    id={LAYOUT_ROOT_ID}
+    className='relative h-[844px] w-full shrink-0 shadow-xl'
+  >
     <Story />
   </div>
 );
