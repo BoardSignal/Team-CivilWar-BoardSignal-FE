8ddb5ec6e716d6b0686456edb609386157295fe0
Seongbin Kim <seongbin9786@gmail.com>
2024-02-21 14:50:52 +0900

    fix: TabBar의 Story 분류 오류 수정 (common -> components)

--- src/stories/components/TabBar.stories.tsx
+++ src/stories/components/TabBar.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import TabBar from '@/components/TabBar';
 
 const meta: Meta<typeof TabBar.Container> = {
-  title: 'common/TabBar',
+  title: 'components/TabBar',
   tags: ['autodocs'],
   component: TabBar.Container,
 };
