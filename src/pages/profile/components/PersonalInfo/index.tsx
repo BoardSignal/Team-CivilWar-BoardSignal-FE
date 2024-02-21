555c6c7445e7ced6b90bc96ec8a817445a7ac0e6
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-02-21 21:55:20 +0900

    refactor: 이미지 빈값처리 및 url이동 추가

--- src/pages/profile/components/PersonalInfo/index.tsx
+++ src/pages/profile/components/PersonalInfo/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import Icon from '@/components/Icon';
 
 interface PersonalInfoProps {
@@ -15,20 +17,28 @@ const PersonalInfo = ({
   ageGroup,
   profileImageUrl,
 }: PersonalInfoProps) => {
+  const DEFAULT_PROFILE_IMAGE_URL = 'https://picsum.photos/200/200';
+  const navigate = useNavigate();
+
   return (
     <div className='flex flex-col items-center gap-2 px-4'>
       <img
-        src={profileImageUrl}
+        src={profileImageUrl || DEFAULT_PROFILE_IMAGE_URL}
         alt='아바타 이미지'
         className='h-[150px] w-[150px] rounded-full object-cover'
       />
       <div className='flex flex-col items-center gap-1'>
         <span className='text-lg font-bold text-gray-accent1'>{nickname}</span>
         <span className='text-sm text-gray-accent2'>{`${ageGroup} ${gender}`}</span>
-        <div className='flex gap-1'>
+        <div
+          className='flex cursor-pointer gap-1'
+          onClick={() => navigate('end-game')}
+        >
           <Icon id='dice-line' className='text-indigo-500' />
           <span className='text-sm font-bold text-indigo-500'>Signal</span>
-          <span className='text-sm font-bold text-gray-accent1'>{signal}</span>
+          <span className='cursor-default text-sm font-bold text-gray-accent1'>
+            {signal}
+          </span>
         </div>
       </div>
     </div>
