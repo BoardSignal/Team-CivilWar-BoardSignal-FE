28235a5f9e698a56af0c62d9e033f0e882d41d34
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-02-21 11:27:45 +0900

    refactor: 시그널 온도별 색상 값 변경

--- src/pages/profile/components/Temperature/index.tsx
+++ src/pages/profile/components/Temperature/index.tsx
@@ -6,16 +6,33 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '@/components/Tooltip';
+import { cn } from '@/utils/cn';
 
 interface TemperatureProps {
   value: number;
 }
 const Temperature = ({ value = 50.3 }: TemperatureProps) => {
+  const temperatureRange = (value: number) => {
+    if (value < 12.5)
+      return { text: 'text-manner-accent7', background: 'bg-manner-accent7' };
+    if (value <= 30)
+      return { text: 'text-manner-accent6', background: 'bg-manner-accent6' };
+    if (value <= 36.5)
+      return { text: 'text-manner-accent5', background: 'bg-manner-accent5' };
+    if (value <= 50.5)
+      return { text: 'text-manner-accent4', background: 'bg-manner-accent4' };
+    if (value <= 65.5)
+      return { text: 'text-manner-accent3', background: 'bg-manner-accent3' };
+    if (value <= 88)
+      return { text: 'text-manner-accent2', background: 'bg-manner-accent2' };
+    return { text: 'text-manner-accent1', background: 'bg-manner-accent1' };
+  };
+
   return (
     <div>
       <TooltipProvider>
         <Tooltip>
-          <TooltipTrigger asChild>
+          <TooltipTrigger>
             <span className='flex w-fit gap-1 text-xs text-gray-accent1 underline'>
               {'시그널온도'}
               <img src={informationLine} alt='정보' className='h-4 w-4' />
@@ -28,13 +45,18 @@ const Temperature = ({ value = 50.3 }: TemperatureProps) => {
         </Tooltip>
       </TooltipProvider>
       <div className='flex flex-col gap-1'>
-        <span className='ml-auto flex items-center gap-1 text-sm font-bold text-blue-accent1'>
+        <span
+          className={cn(
+            'ml-auto flex items-center gap-1 text-sm font-bold',
+            temperatureRange(value).text,
+          )}
+        >
           {`${value}˚C`}
           <img src={smileFace} alt='Signal' className='h-5 w-5' />
         </span>
         <div className='h-2 overflow-hidden rounded-2xl bg-gray-accent7'>
           <div
-            className='h-full bg-blue-accent1'
+            className={cn('h-full', temperatureRange(value).background)}
             style={{ width: `${value}%` }}
           ></div>
         </div>
