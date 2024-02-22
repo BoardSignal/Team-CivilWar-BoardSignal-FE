518707e061c2fa126277c77b77457d69095f2b19
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-02-22 10:41:28 +0900

    style: 보드게임 이미지 크기 수정

--- src/pages/boardGameDetail/components/Info/index.tsx
+++ src/pages/boardGameDetail/components/Info/index.tsx
@@ -1,5 +1,6 @@
 import Chip from '@/components/Chip';
 import Icon from '@/components/Icon';
+import Ripple from '@/components/Ripple';
 
 const DEFAULT_BOARD_GAME_IMAGE_URL = 'https://picsum.photos/200/200';
 
@@ -30,7 +31,7 @@ const Info = ({
     <>
       <img
         src={imageUrl || DEFAULT_BOARD_GAME_IMAGE_URL}
-        className='h-full w-full min-w-[350px] max-w-[450px] object-cover'
+        className='max-h-[240px] min-w-[350px] max-w-[450px] object-cover'
       />
       <div className='flex flex-col items-center gap-3 border-b border-gray-accent7 py-5'>
         <div className='flex flex-col items-center gap-1'>
@@ -58,9 +59,9 @@ const Info = ({
           </span>
         </span>
         <div className='flex flex-col items-center gap-1'>
-          <div className='flex h-8 w-8 items-center justify-center rounded-full border border-gray-accent3 '>
+          <Ripple className='flex h-8 w-8 items-center justify-center rounded-full border border-gray-accent3 '>
             <Icon id='bookmark-line' size={16} className='text-gray-accent3' />
-          </div>
+          </Ripple>
           <span className='text-xs text-gray-accent1'>{wishCount}</span>
         </div>
       </div>
