65e431e5074d11e96920520eb2b2940b8f34a7ea
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-02-22 09:54:37 +0900

    feat: 보드게임 설명 구현

---
+++ src/pages/boardGameDetail/components/Description/index.tsx
@@ -0,0 +1,13 @@
+interface DescriptionProps {
+  description: string;
+}
+const Description = ({ description }: DescriptionProps) => {
+  return (
+    <div className='text-accent-gray1 flex flex-col gap-1.5 border-b border-gray-accent7 p-4'>
+      <span className='font-bold'>설명</span>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default Description;
---
+++ src/stories/pages/boardGameDetail/Description.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Description from '@/pages/boardGameDetail/components/Description';
+
+const meta: Meta<typeof Description> = {
+  title: 'Pages/boardGameDetail/Description',
+  tags: ['autodocs'],
+  component: Description,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Description>;
+
+export const Default: Story = {
+  args: {
+    description:
+      '스플렌더는 칩 수집 및 카드 개발 게임입니다. 플레이어는 르네상스 시대의 상인이 되어 보석 광산, 운송 수단, 상점 등을 구매하고 명성 포인트를 많이 획득해야 합니다. 충분히 부유하다면 귀족의 방문을 받을 수도 있으며, 이 경우 명성이 더욱 높아질 것입니다.',
+  },
+};
