ec39deaf626f1c860868765b096b72b7ec3e94ff
lja0395 <kittyja1023@naver.com>
2024-02-27 18:21:16 +0900

    refactor: 코드리뷰반영

--- src/stories/components/Label.stories.tsx
+++ src/stories/components/Label.stories.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Label from '@/components/Label';
+import Textarea from '@/components/TextArea';
 import TextInput from '@/components/TextInput';
 
 const meta: Meta<typeof Label> = {
@@ -19,18 +20,32 @@ export const DefaultTemplate = (args: Story) => {
   const [value, setValue] = useState('');
 
   return (
-    <Label
-      currentLength={value.length}
-      title='성별'
-      width='300px'
-      maxLength={500}
-      {...args}
-    >
-      <TextInput
-        placeholder='남성'
-        disabled={true}
-        onChange={e => setValue(e.target.value)}
-      />
-    </Label>
+    <>
+      <Label
+        currentLength={value.length}
+        title='성별'
+        width='300px'
+        maxLength={500}
+        {...args}
+      >
+        <TextInput
+          placeholder='남성'
+          disabled={true}
+          onChange={e => setValue(e.target.value)}
+        />
+      </Label>
+      <Label
+        currentLength={value.length}
+        title='모임 설명'
+        width='300px'
+        maxLength={500}
+        {...args}
+      >
+        <Textarea
+          placeholder='모임 설명을 작성해주세요.'
+          onChange={e => setValue(e.target.value)}
+        />
+      </Label>
+    </>
   );
 };
