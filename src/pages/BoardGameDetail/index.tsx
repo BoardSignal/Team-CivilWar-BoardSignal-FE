bacddf6c37a44d02ee957f7f897b96e1e3a3ee52
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-02-29 01:09:57 +0900

    refactor: 보드게임 공략 props변경

--- src/pages/BoardGameDetail/index.tsx
+++ src/pages/BoardGameDetail/index.tsx
@@ -4,6 +4,7 @@ import GameDescription from './components/GameDescription';
 import GamePlayDetail from './components/GamePlayDetail';
 import GameSummary from './components/GameSummary';
 import GameWish from './components/GameWish';
+import BoardGameTip from './components/Tips';
 
 const DummyData = {
   name: '스플렌더',
@@ -17,6 +18,41 @@ const DummyData = {
   categories: ['전략', '카드'],
   description:
     '스플렌더는 칩 수집 및 카드 개발 게임입니다. 플레이어는 르네상스 시대의 상인이 되어 보석 광산, 운송 수단, 상점 등을 구매하고 명성 포인트를 많이 획득해야 합니다. 충분히 부유하다면 귀족의 방문을 받을 수도 있으며, 이 경우 명성이 더욱 높아질 것입니다.',
+  tips: [
+    {
+      tipId: 1,
+      nickname: 'injuniing',
+      profileImageUrl: '',
+      createdAt: '2024-01-31 14:30',
+      content: '개꿀팁 갑니다',
+    },
+    {
+      tipId: 2,
+      nickname: 'injuniing',
+      profileImageUrl: '',
+      createdAt: '2024-02-23 10:30',
+      content: `
+        개꿀팁 갑니다zㅋㅋㅋㅋ 
+        개꿀팁 갑니다 개꿀팁 갑니다 
+        개꿀팁 갑니다 ㅋㅋㅋ 
+        개꿀팁 갑니다 
+        개꿀팁 갑니다`,
+    },
+    {
+      tipId: 2,
+      nickname: 'Hober',
+      profileImageUrl: '',
+      createdAt: '2023-02-23 10:30',
+      content: `
+        적은 인원일 땐 귀족 카드 노리세요. 
+        적은 인원일 땐 귀족 카드 노리세요. 
+        적은 인원일 땐 귀족 카드 노리세요. 
+        적은 인원일 땐 귀족 카드 노리세요. 
+        적은 인원일 땐 귀족 카드 노리세요. 
+        적은 인원일 땐 귀족 카드 노리세요. 
+        `,
+    },
+  ],
 };
 
 const BoardGameDetailPage = () => {
@@ -31,6 +67,7 @@ const BoardGameDetailPage = () => {
     difficulty,
     categories,
     description,
+    tips,
   } = DummyData;
 
   const boardGameImageUrl = imageUrl || 'https://picsum.photos/200/200';
@@ -44,20 +81,23 @@ const BoardGameDetailPage = () => {
         </TabBar.Left>
       </TabBar.Container>
       <img src={boardGameImageUrl} className='h-60' alt={name} />
-      <div className='flex flex-col items-center gap-3 border-b border-gray-accent7 py-5'>
-        <GameSummary name={name} categories={categories} />
-        <GamePlayDetail
-          gamePlayDetail={{
-            difficulty,
-            minParticipants,
-            maxParticipants,
-            fromPlayTime,
-            toPlayTime,
-          }}
-        />
-        <GameWish wishCount={wishCount} />
+      <div className='overflow-y-auto'>
+        <div className='flex flex-col items-center gap-3 border-b border-gray-accent7 py-5'>
+          <GameSummary name={name} categories={categories} />
+          <GamePlayDetail
+            gamePlayDetail={{
+              difficulty,
+              minParticipants,
+              maxParticipants,
+              fromPlayTime,
+              toPlayTime,
+            }}
+          />
+          <GameWish wishCount={wishCount} />
+        </div>
+        <GameDescription description={description} />
+        <BoardGameTip tips={tips} />
       </div>
-      <GameDescription description={description} />
     </div>
   );
 };
