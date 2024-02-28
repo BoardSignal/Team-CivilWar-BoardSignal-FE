92a194571a74f0f73aafe23fcf3d19da13a0e2aa
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-02-28 19:59:35 +0900

    fix: 빌드오류 해결

--- src/stories/pages/BoardGameDetail/GamePlayDetail.stories.tsx
+++ src/stories/pages/BoardGameDetail/GamePlayDetail.stories.tsx
@@ -14,10 +14,12 @@ type Story = StoryObj<typeof GamePlayDetail>;
 
 export const Default: Story = {
   args: {
-    difficulty: '보통',
-    minParticipants: 2,
-    maxParticipants: 4,
-    fromPlayTime: 30,
-    toPlayTime: 60,
+    gamePlayDetail: {
+      difficulty: '보통',
+      minParticipants: 2,
+      maxParticipants: 4,
+      fromPlayTime: 30,
+      toPlayTime: 60,
+    },
   },
 };
