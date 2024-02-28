7af8ee8eb9bcd5b5aa481b3092628d5b5f440254
lja0395 <kittyja1023@naver.com>
2024-02-28 22:43:50 +0900

    refactor: 스타일 변경

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -3,7 +3,7 @@ import { ComponentPropsWithoutRef } from 'react';
 const Textarea = ({ ...props }: ComponentPropsWithoutRef<'textarea'>) => {
   return (
     <textarea
-      className='h-[180px] w-full resize-none overscroll-contain rounded-lg border border-gray-accent7 p-5 outline-none focus:border-gray-accent2'
+      className='h-[180px] w-full resize-none appearance-none overscroll-contain rounded-lg border border-gray-accent7 p-5 outline-gray-accent2'
       {...props}
     />
   );
