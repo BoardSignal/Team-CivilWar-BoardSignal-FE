c0f0ca43dba01365c0efab5f3919dec4cde12b92
Seongbin Kim <seongbin9786@gmail.com>
2024-03-01 12:46:24 +0900

    fix: 기존 Mutation에 변경된 Response 처리 추가

--- src/apis/kakaoLogin.ts
+++ src/apis/kakaoLogin.ts
@@ -18,5 +18,12 @@ export const usePostKakaoLogin = () => {
     mutationFn: postKakaoLogin,
   });
 
-  return mutateAsync;
+  return async () => {
+    const { isBadRequest, data } = await mutateAsync();
+    if (isBadRequest) {
+      throw new Error('카카오 로그인 과정 중 예상치 못한 오류가 발생했어요.');
+    }
+
+    return data;
+  };
 };
--- src/apis/naverLogin.ts
+++ src/apis/naverLogin.ts
@@ -17,5 +17,12 @@ export const usePostNaverLogin = () => {
     mutationFn: postNaverLogin,
   });
 
-  return mutateAsync;
+  return async () => {
+    const { isBadRequest, data } = await mutateAsync();
+    if (isBadRequest) {
+      throw new Error('네이버 로그인 과정 중 예상치 못한 오류가 발생했어요.');
+    }
+
+    return data;
+  };
 };
