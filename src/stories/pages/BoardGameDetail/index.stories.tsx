8b750c4d08018c23ca7428158c2c5e07ae383b0f
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-01 23:22:28 +0900

    fix: storybook decorator 추가

--- src/stories/pages/BoardGameDetail/index.stories.tsx
+++ src/stories/pages/BoardGameDetail/index.stories.tsx
@@ -1,11 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import BoardGameDetailPage from '@/pages/BoardGameDetail';
+import { CommonPageLayoutDecorator } from '@/stories/CommonPageLayoutDecorator';
 
 const meta: Meta<typeof BoardGameDetailPage> = {
   title: 'pages/BoardGameDetail/page',
   tags: ['autodocs'],
   component: BoardGameDetailPage,
+  decorators: [CommonPageLayoutDecorator],
 };
 
 export default meta;
--- src/stories/pages/MyProfile/index.stories.tsx
+++ src/stories/pages/MyProfile/index.stories.tsx
@@ -1,10 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import MyProfile from '@/pages/MyProfile';
+import { CommonPageLayoutDecorator } from '@/stories/CommonPageLayoutDecorator';
 
 const meta: Meta<typeof MyProfile> = {
   title: 'pages/MyProfile/page',
   tags: ['autodocs'],
+  decorators: [CommonPageLayoutDecorator],
   component: MyProfile,
 };
 
