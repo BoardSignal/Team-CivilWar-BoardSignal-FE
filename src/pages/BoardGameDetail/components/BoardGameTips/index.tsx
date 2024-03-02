978cb1b077d4cbeedf7fc0813ab0c8570321008d
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-03 01:49:17 +0900

    refactor: 코드리뷰반영

--- src/pages/BoardGameDetail/components/BoardGameTips/index.tsx
+++ src/pages/BoardGameDetail/components/BoardGameTips/index.tsx
@@ -1,6 +1,6 @@
+import Button from '@/components/Button';
 import Icon from '@/components/Icon';
-import Ripple from '@/components/Ripple';
-import { getTimeDifferenceString } from '@/utils/getTimeDifferenceString';
+import { convertToRelativeTime } from '@/utils/convertToRelativeTime';
 
 interface TipProps {
   tipId: number;
@@ -8,10 +8,13 @@ interface TipProps {
   profileImageUrl: string;
   createdAt: string;
   content: string;
+  likeCount: number;
 }
 
-const Tip = ({ nickname, profileImageUrl, createdAt, content }: TipProps) => {
-  const DEFAULT_PROFILE_IMAGE_URL = 'https://picsum.photos/200/200';
+const DEFAULT_PROFILE_IMAGE_URL = 'https://picsum.photos/200/200';
+
+const Tip = ({ tip }: { tip: TipProps }) => {
+  const { nickname, profileImageUrl, createdAt, content, likeCount } = tip;
 
   return (
     <div className='flex gap-2 border-b border-gray-accent7 p-4'>
@@ -21,17 +24,17 @@ const Tip = ({ nickname, profileImageUrl, createdAt, content }: TipProps) => {
       />
       <div className='flex w-full flex-col gap-2'>
         <div className='flex items-center justify-between'>
-          <span className='text-accent1 text-sm font-bold'>{nickname}</span>
+          <span className='text-sm font-bold text-gray-accent1'>
+            {nickname}
+          </span>
           <span className='text-[10px] text-gray-accent3'>
-            {getTimeDifferenceString(createdAt)}
+            {convertToRelativeTime(createdAt)}
           </span>
         </div>
         <p className='text-gray-accent1'>{content}</p>
         <div className='flex items-center gap-1'>
-          <Ripple>
-            <Icon id='thumb-up-line' size={16} className='text-gray-accent3' />
-          </Ripple>
-          <span className='text-xs text-gray-accent1'>2</span>
+          <Icon id='thumb-up-line' size={16} className='text-gray-accent3' />
+          <span className='text-xs text-gray-accent1'>{likeCount}</span>
         </div>
       </div>
     </div>
@@ -47,24 +50,13 @@ const BoardGameTips = ({ tips }: BoardGameTipsProps) => {
     <>
       <div className='flex items-center justify-between p-4'>
         <span className='font-bold'>공략</span>
-        <Ripple className='flex w-fit items-center gap-0.5 rounded-3xl bg-primary p-3 pl-2 text-white'>
+        <Button className='flex w-fit items-center gap-0.5 rounded-3xl bg-primary p-3 pl-2 text-white'>
           <Icon id='add-line' size={17} />
           <span className='text-xs font-bold'>작성하기</span>
-        </Ripple>
+        </Button>
       </div>
       {tips.map(tip => {
-        const { nickname, profileImageUrl, createdAt, content, tipId } = tip;
-
-        return (
-          <Tip
-            key={tipId}
-            tipId={tipId}
-            nickname={nickname}
-            profileImageUrl={profileImageUrl}
-            createdAt={createdAt}
-            content={content}
-          />
-        );
+        return <Tip tip={tip} />;
       })}
     </>
   );
