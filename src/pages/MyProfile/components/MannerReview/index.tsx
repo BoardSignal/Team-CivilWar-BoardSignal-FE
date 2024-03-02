ae2cf1da85737618bb79987fb17dc6c52ea19c0c
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-02 22:14:04 +0900

    feat: 더미데이터 제거 및 실제 데이터 추가

--- src/pages/MyProfile/components/MannerReview/index.tsx
+++ src/pages/MyProfile/components/MannerReview/index.tsx
@@ -1,12 +1,8 @@
+import type { Review } from '@/apis/userProfile';
 import Icon from '@/components/Icon';
 
 interface ReviewProps {
-  reviews: ReviewContentProps[];
-}
-
-interface ReviewContentProps {
-  reviewScore: number;
-  content: string;
+  reviews: Review[];
 }
 
 const MannerReview = ({ reviews }: ReviewProps) => {
@@ -14,24 +10,20 @@ const MannerReview = ({ reviews }: ReviewProps) => {
     <div className='flex flex-col'>
       <span className='py-2 font-bold'>매너 리뷰</span>
       <div className='flex flex-col gap-2 px-1'>
-        {reviews.map(({ reviewScore, content }) => (
-          <ReviewContent
-            content={content}
-            reviewScore={reviewScore}
-            key={content}
-          />
+        {reviews.map(({ score, content }) => (
+          <ReviewContent content={content} score={score} key={content} />
         ))}
       </div>
     </div>
   );
 };
 
-const ReviewContent = ({ reviewScore, content }: ReviewContentProps) => {
+const ReviewContent = ({ score, content }: Review) => {
   return (
     <div className='flex'>
       <div className='flex w-[60px] items-center gap-2'>
         <Icon id='group-line' />
-        <span className='text-gray-accent1'>{reviewScore}</span>
+        <span className='text-gray-accent1'>{score}</span>
       </div>
       <div className='rounded-tl-0 flex items-center rounded-bl-lg rounded-br-lg rounded-tr-lg bg-gray-accent7 p-2'>
         <span className='text-gray-accent1'>{content}</span>
--- src/pages/MyProfile/index.tsx
+++ src/pages/MyProfile/index.tsx
@@ -1,7 +1,9 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import { useGetUserProfilesApi } from '@/apis/userProfile';
+import type { UserProfileResponse } from '@/apis/userProfile';
 import Button from '@/components/Button';
+import { GNB } from '@/components/GNB';
 import Icon from '@/components/Icon';
 import TabBar from '@/components/TabBar';
 
@@ -10,53 +12,39 @@ import PersonalInfo from './components/PersonalInfo';
 import PreferCategory from './components/PreferCategory';
 import SignalTemperature from './components/SignalTemperature';
 
-const PROFILE_PAGE_DUMMY_DATA = {
-  personalInfo: {
-    nickname: '네이밍 못짓는 호민',
-    signalCount: 15,
-    profileImageUrl: '',
-    ageGroup: '20대',
-    gender: '남성',
-  },
-  signalTemperature: 86.3,
-  reviews: [
-    {
-      content: '시간 약속을 잘 지켜요.',
-      reviewScore: 12,
-    },
-    {
-      content: '친절하고 매너가 좋아요.',
-      reviewScore: 9,
-    },
-    {
-      content: '응답이 빨라요.',
-      reviewScore: 4,
-    },
-  ],
-  categories: ['배팅/전략', '카드경매', '심리', '블러핑', '마피아', '카드빙고'],
-  wishCount: 3,
-};
-
 const WishGameList = ({ wishGamesCount }: { wishGamesCount: number }) => {
-  const navigate = useNavigate();
   const { userId } = useParams();
-  const goToWishGamePage = () => {
-    navigate(`/wish-game/${userId}`);
-  };
 
   return (
-    <Button
-      onClick={goToWishGamePage}
-      className='flex h-auto gap-4 border-b border-gray-accent7 p-4'
-    >
-      <div className='flex w-full justify-between'>
-        <span className='flex gap-1'>
-          <span className='font-bold text-gray-accent1'>찜한 게임 목록</span>
-          <span>{wishGamesCount}</span>
-        </span>
-        <Icon id='arrow-right-line' className='cursor-pointer'></Icon>
-      </div>
-    </Button>
+    <Link to={`/wish-game/${userId}`}>
+      <Button className='flex h-auto gap-4 border-b border-gray-accent7 p-4'>
+        <div className='flex w-full justify-between'>
+          <span className='flex gap-1'>
+            <span className='font-bold text-gray-accent1'>찜한 게임 목록</span>
+            <span className='text-black'>{wishGamesCount}</span>
+          </span>
+          <Icon id='arrow-right-line' className='cursor-pointer'></Icon>
+        </div>
+      </Button>{' '}
+    </Link>
+  );
+};
+
+const EndedGatheringList = ({ endedGame }: { endedGame: number }) => {
+  return (
+    <Link to='/end-games'>
+      <Button className='flex h-auto gap-4 border-b border-gray-accent7 p-4'>
+        <div className='flex w-full justify-between'>
+          <span className='flex gap-1'>
+            <span className='font-bold text-gray-accent1'>
+              종료된 게임 목록
+            </span>
+            <span className='text-black'>{endedGame}</span>
+          </span>
+          <Icon id='arrow-right-line' className='cursor-pointer'></Icon>
+        </div>
+      </Button>
+    </Link>
   );
 };
 
@@ -73,12 +61,32 @@ const EditProfileButton = () => {
 };
 
 const MyProfilePage = () => {
-  const { reviews, signalTemperature, personalInfo, categories, wishCount } =
-    PROFILE_PAGE_DUMMY_DATA;
-
   const { userId } = useParams() as { userId: string };
   const { data, isPending, isError } = useGetUserProfilesApi(userId);
-  console.log(data, isPending, isError);
+
+  if (isPending) return <div>Loading...</div>;
+  if (isError) return <div>Error...</div>;
+
+  const {
+    ageGroup,
+    gender,
+    isProfileManager,
+    mannerScore,
+    preferCategories,
+    profileImageUrl,
+    reviews,
+    signal,
+    wishCount,
+    nickname,
+  } = data as UserProfileResponse;
+
+  const personalInfo = {
+    ageGroup,
+    nickname,
+    signalCount: signal,
+    gender,
+    profileImageUrl,
+  };
 
   return (
     <div className='flex h-full flex-col'>
@@ -94,14 +102,16 @@ const MyProfilePage = () => {
       </TabBar.Container>
       <div className='grow overflow-y-auto'>
         <PersonalInfo personalInfo={personalInfo} />
-        <EditProfileButton />
+        {isProfileManager && <EditProfileButton />}
         <div className='flex flex-col gap-2 border-b border-gray-accent7 p-4'>
-          <SignalTemperature value={signalTemperature} />
+          <SignalTemperature value={mannerScore} />
           <MannerReview reviews={reviews} />
         </div>
-        <PreferCategory categories={categories} />
+        <PreferCategory categories={preferCategories} />
         <WishGameList wishGamesCount={wishCount} />
+        <EndedGatheringList endedGame={signal} />
       </div>
+      {isProfileManager && <GNB />}
     </div>
   );
 };
