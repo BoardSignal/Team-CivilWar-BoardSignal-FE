a60d939b54a574457d6671550127683b2ebb6aaa
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-03 20:44:58 +0900

    feat: 찜했는지에 따른 버튼렌더링 변경

--- src/pages/BoardGameDetail/components/GameWish/index.tsx
+++ src/pages/BoardGameDetail/components/GameWish/index.tsx
@@ -1,21 +1,37 @@
+import { useState } from 'react';
+
 import { useParams } from 'react-router-dom';
 
 import Button from '@/components/Button';
 import Icon from '@/components/Icon';
 import { useBoardGameWish } from '@/pages/BoardGameDetail/hooks/useBoardGameWish';
+import { cn } from '@/utils/cn';
 
 const GameWish = ({ wishCount }: { wishCount: number }) => {
   const { boardGameId } = useParams() as { boardGameId: string };
+  const [isBoardGameWished, setIsBoardGameWished] = useState(false);
 
   const { postBoardGameWish } = useBoardGameWish(boardGameId);
+
+  const clickedWishButton = () => {
+    postBoardGameWish();
+    setIsBoardGameWished(!isBoardGameWished);
+  };
 
   return (
     <div className='flex flex-col items-center gap-1'>
       <Button
-        onClick={postBoardGameWish}
-        className='flex h-8 w-8 items-center justify-center rounded-full border border-gray-accent3 '
+        onClick={clickedWishButton}
+        className={cn(
+          'flex h-8 w-8 items-center justify-center rounded-full border border-gray-accent3',
+          isBoardGameWished && 'bg-primary',
+        )}
       >
-        <Icon id='bookmark-line' size={16} className='text-gray-accent3' />
+        <Icon
+          id={isBoardGameWished ? 'bookmark-fill' : 'bookmark-line'}
+          size={16}
+          className={cn('text-gray-accent3', isBoardGameWished && 'text-white')}
+        />
       </Button>
       <span className='text-xs text-gray-accent1'>{wishCount}</span>
     </div>
