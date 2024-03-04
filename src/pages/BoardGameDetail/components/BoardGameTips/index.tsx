9e4220a5db5342687ee9fabc803b22f77070912a
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-04 21:03:54 +0900

    feat: 내 공략관련 구현 및 코드분리

--- src/pages/BoardGameDetail/components/BoardGameTips/index.tsx
+++ src/pages/BoardGameDetail/components/BoardGameTips/index.tsx
@@ -1,46 +1,18 @@
 import { Link } from 'react-router-dom';
 
-import type { MyTip, Tip } from '@/apis/boardGameDetail';
+import type { Tip } from '@/apis/boardGameDetail';
 import Button from '@/components/Button';
 import Icon from '@/components/Icon';
-import { convertToRelativeTime } from '@/utils/convertToRelativeTime';
+
+import MyTipItem from './MyTipItem';
+import TipItem from './TipItem';
 
 interface BoardGameTipsProps {
   tips: Tip[];
   name: string;
   boardGameId: string;
-  myTip: MyTip | null;
+  myTip: Tip | null;
 }
-
-const DEFAULT_PROFILE_IMAGE_URL = 'https://picsum.photos/200/200';
-
-const Tip = ({ tip }: { tip: Tip }) => {
-  const { nickname, profileImageUrl, createdAt, content, likeCount } = tip;
-
-  return (
-    <div className='flex gap-2 border-b border-gray-accent7 p-4'>
-      <img
-        src={profileImageUrl || DEFAULT_PROFILE_IMAGE_URL}
-        className='h-10 w-10 rounded-full object-cover'
-      />
-      <div className='flex w-full flex-col gap-2'>
-        <div className='flex items-center justify-between'>
-          <span className='text-sm font-bold text-gray-accent1'>
-            {nickname}
-          </span>
-          <span className='text-[10px] text-gray-accent3'>
-            {convertToRelativeTime(createdAt)}
-          </span>
-        </div>
-        <p className='text-gray-accent1'>{content}</p>
-        <div className='flex items-center gap-1'>
-          <Icon id='thumb-up-line' size={16} className='text-gray-accent3' />
-          <span className='text-xs text-gray-accent1'>{likeCount}</span>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const BoardGameTips = ({
   tips,
@@ -50,19 +22,21 @@ const BoardGameTips = ({
 }: BoardGameTipsProps) => {
   return (
     <>
-      {!myTip && (
+      {!myTip ? (
         <div className='flex items-center justify-between p-4'>
           <span className='font-bold'>공략</span>
           <Link to={`board-games/${boardGameId}/${name}/tip/create`}>
             <Button className='flex w-fit items-center gap-0.5 rounded-3xl bg-primary p-3 pl-2 text-white'>
-              <Icon id='add-line' size={17} />
+              <Icon id='add-line' size={16} />
               <span className='text-xs font-bold'>작성하기</span>
             </Button>
           </Link>
         </div>
+      ) : (
+        <MyTipItem tip={myTip} />
       )}
       {tips.map(tip => (
-        <Tip tip={tip} key={tip.tipId} />
+        <TipItem tip={tip} key={tip.tipId} />
       ))}
     </>
   );
