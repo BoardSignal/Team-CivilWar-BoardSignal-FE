c0ed2754f590cd1a25b9c5e5483bcaa12bb24d0e
lja0395 <99376069+lja0395@users.noreply.github.com>
2024-03-05 22:41:44 +0900

    [#69] Feat: 공통컴포넌트 - UserProfile (#72)

--- src/pages/Profile/components/SignalTemperature/index.tsx
+++ src/pages/Profile/components/SignalTemperature/index.tsx
@@ -2,26 +2,11 @@ import smileFace from '@/assets/smiling-face.png';
 import Icon from '@/components/Icon';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/Popover';
 import { cn } from '@/utils/cn';
+import getColorByTemperature from '@/utils/getColorByTemperature';
 
 interface TemperatureProps {
   value: number;
 }
-
-const getColorByTemperature = (value: number) => {
-  if (value < 12.5)
-    return { text: 'text-manner-accent7', background: 'bg-manner-accent7' };
-  if (value <= 30)
-    return { text: 'text-manner-accent6', background: 'bg-manner-accent6' };
-  if (value <= 36.5)
-    return { text: 'text-manner-accent5', background: 'bg-manner-accent5' };
-  if (value <= 50.5)
-    return { text: 'text-manner-accent4', background: 'bg-manner-accent4' };
-  if (value <= 65.5)
-    return { text: 'text-manner-accent3', background: 'bg-manner-accent3' };
-  if (value <= 88)
-    return { text: 'text-manner-accent2', background: 'bg-manner-accent2' };
-  return { text: 'text-manner-accent1', background: 'bg-manner-accent1' };
-};
 
 const SignalTemperature = ({ value = 50.3 }: TemperatureProps) => {
   const { text, background } = getColorByTemperature(value);
