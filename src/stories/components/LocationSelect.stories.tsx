2533fa02048e472ab761777b87de2b3d7ae48168
Seongbin Kim <seongbin9786@gmail.com>
2024-03-05 16:22:01 +0900

    docs: LocationSelect 스토리북 추가

---
+++ src/stories/components/LocationSelect.stories.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+import type { Args, Meta, StoryObj } from '@storybook/react';
+
+import Label from '@/components/Label';
+import LocationSelect from '@/components/LocationSelect';
+
+import { CommonPageLayoutDecorator } from '../CommonPageLayoutDecorator';
+
+const meta: Meta<typeof LocationSelect> = {
+  title: 'components/LocationSelect',
+  tags: ['autodocs'],
+  component: LocationSelect,
+  decorators: [
+    Story => (
+      <div className='p-4'>
+        <Story />
+      </div>
+    ),
+    CommonPageLayoutDecorator,
+  ],
+  render: function ExampleForm(args: Args) {
+    const [value, setValue] = useState('');
+
+    return (
+      <Label title='모임 장소' isRequired>
+        <LocationSelect value={value} onChange={setValue} {...args} />
+      </Label>
+    );
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof LocationSelect>;
+
+export const DefaultTemplate: Story = {};
