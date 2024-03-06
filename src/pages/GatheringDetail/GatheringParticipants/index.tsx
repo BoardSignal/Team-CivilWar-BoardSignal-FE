eed17825f03996b491f004d21e6bb07961fdfe20
lja0395 <kittyja1023@naver.com>
2024-03-06 22:06:51 +0900

    feat: 모임 상세페이지 참가자 목록 구현

---
+++ src/pages/GatheringDetail/GatheringParticipants/index.tsx
@@ -0,0 +1,34 @@
+import UserProfile from '@/components/UserProfile';
+
+export interface Participants {
+  userId: number;
+  nickname: string;
+  ageGroup: string;
+  isLeader?: boolean;
+  signalTemperature: number;
+}
+
+interface GatheringParticipantsProps {
+  participants: Participants[];
+}
+
+const GatheringParticipants = ({
+  participants,
+}: GatheringParticipantsProps) => {
+  return (
+    <section className='grow overflow-y-auto'>
+      <ul>
+        {participants.map(participant => (
+          <UserProfile
+            key={participant.userId}
+            nickname={participant.nickname}
+            ageGroup={participant.ageGroup}
+            signalTemperature={participant.signalTemperature}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default GatheringParticipants;
