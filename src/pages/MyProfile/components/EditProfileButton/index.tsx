8b1f2a7be42e324c6dfa025f54364304ddf8c5cf
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-07 01:16:02 +0900

    fix: 오타수정

--- src/pages/MyProfile/components/EditProfileButton/index.tsx
+++ src/pages/MyProfile/components/EditProfileButton/index.tsx
@@ -9,7 +9,7 @@ interface EditProfileButtonProps {
 const EditProfileButton = ({ userId }: EditProfileButtonProps) => {
   return (
     <div className='border-b border-gray-accent7 p-4'>
-      <Link to={`/users/edit${userId}}`}>
+      <Link to={`/users/edit${userId}`}>
         <Button className='rounded-lg bg-primary text-white'>
           프로필 수정
         </Button>
