28f33961f8a70b1bac8b31a0730774dba41ada43
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-07 11:08:10 +0900

    feat: reissue 구현

--- src/apis/core/authentication.ts
+++ src/apis/core/authentication.ts
@@ -4,7 +4,13 @@ import axios, {
   isAxiosError,
 } from 'axios';
 
-const TOKEN_EXPIRED_MESSAGE = 'Token expired';
+import { AUTH_REISSUE_API_URL } from '@/constants/apiRoutes';
+
+const TOKEN_EXPIRED_MESSAGE = '기한이 만료된 토큰입니다';
+
+interface ReissueResponse {
+  accessToken: string;
+}
 
 export const attachAccessToken = (config: InternalAxiosRequestConfig) => {
   const accessToken = localStorage.getItem('accessToken');
@@ -14,15 +20,30 @@ export const attachAccessToken = (config: InternalAxiosRequestConfig) => {
   return config;
 };
 
-export const refreshExpiredToken = (error: unknown) => {
+export const refreshExpiredToken = async (error: unknown) => {
   if (
     isAxiosError(error) &&
     error.config &&
     error.response?.status === HttpStatusCode.Unauthorized &&
     error.response?.data.message === TOKEN_EXPIRED_MESSAGE
   ) {
-    // TODO: refresh 과정을 수행할 코드의 위치에요.
-    return axios(error.config);
+    const { data } = await axios<ReissueResponse>(
+      `${import.meta.env.VITE_BASE_URL}${AUTH_REISSUE_API_URL}`,
+      {
+        withCredentials: true,
+      },
+    );
+    localStorage.setItem('accessToken', data.accessToken);
+
+    const newAxiosConfig = {
+      ...error.config,
+      headers: {
+        ...error.config.headers,
+        Authorization: `Bearer ${data.accessToken}`,
+      },
+    };
+
+    return axios(newAxiosConfig);
   }
 
   throw error;
