30c6c888fbd336bf6042408c1af1bc2ab9537b76
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-09 16:16:03 +0900

    refactor: 상수코드 변경

--- src/pages/Login/components/LoginButtons.tsx
+++ src/pages/Login/components/LoginButtons.tsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom';
 
 import kakaoLoginButtonImage from '@/assets/kakao-login-button.png';
 import Button from '@/components/Button';
-import { KAKAO } from '@/constants/apiRoutes';
+import { AUTH_LOGIN_KAKAO_URL } from '@/constants/apiRoutes';
 
 // 53.7px로 높이를 지정하는 이유는 원본 이미지의 크기가 그렇기 때문이에요.
 const KAKAO_LOGIN_BUTTON_HEIGHT = 'h-[53.7px]';
 
 export const KakaoLoginButton = () => {
   return (
-    <Link to={`${import.meta.env.VITE_LOGIN_URL}${KAKAO}`}>
+    <Link to={`${import.meta.env.VITE_LOGIN_URL}${AUTH_LOGIN_KAKAO_URL}`}>
       <Button className={KAKAO_LOGIN_BUTTON_HEIGHT}>
         <img src={kakaoLoginButtonImage} alt='네이버 로그인 버튼' />
       </Button>
