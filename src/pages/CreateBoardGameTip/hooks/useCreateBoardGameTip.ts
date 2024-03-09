83887b768db6d8d393b2ffb5567cd17c283dbd02
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-10 01:56:21 +0900

    feat: 보드게임 공략 작성 api 구현

--- src/pages/CreateBoardGameTip/hooks/useCreateBoardGameTip.ts
+++ src/pages/CreateBoardGameTip/hooks/useCreateBoardGameTip.ts
@@ -1,16 +1,21 @@
 import { usePostBoardGameTipApi } from '@/apis/boardGameTip';
-import { CreateTipRequest } from '@/apis/boardGameTip';
+import type { CreateBoardGameTipRequest } from '@/apis/boardGameTip';
+import { showErrorToast } from '@/utils/showToast';
 
-export const useCreateBoardGameTip = () => {
-  const api = usePostBoardGameTipApi();
+import type { OnBoardGameTipCreate } from '../components/CreateBoardGameTipForm';
 
-  return {
-    createBoardGameTip: async ({ boardGameId, content }: CreateTipRequest) => {
-      try {
-        await api({ boardGameId, content });
-      } catch (error) {
-        console.error(error);
-      }
-    },
+const useCreateBoardGameTip = (onCreate: OnBoardGameTipCreate) => {
+  const boardGameTipCreateApi = usePostBoardGameTipApi();
+
+  return async (request: CreateBoardGameTipRequest) => {
+    const { data, isBadRequest } = await boardGameTipCreateApi(request);
+
+    if (isBadRequest) {
+      return showErrorToast(data.message);
+    }
+
+    onCreate();
   };
 };
+
+export default useCreateBoardGameTip;
