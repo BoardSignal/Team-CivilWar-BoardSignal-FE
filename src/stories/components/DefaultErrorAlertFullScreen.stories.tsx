1ad0217fbfaaee289d8e2b1daa53cb48e3b2d913
Seongbin Kim <seongbin9786@gmail.com>
2024-03-13 17:23:20 +0900

    fix: prop 변화를 스토리에 반영

--- src/stories/components/DefaultErrorAlertFullScreen.stories.tsx
+++ src/stories/components/DefaultErrorAlertFullScreen.stories.tsx
@@ -1,6 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import DefaultErrorAlertFullScreen from '@/components/ErrorAlertFullScreen/DefaultErrorAlertFullScreen';
+import {
+  DEFAULT_ERROR_MESSAGE,
+  DEFAULT_ERROR_TITLE,
+} from '@/constants/messages/error';
 
 import { CommonPageLayoutDecorator } from '../CommonPageLayoutDecorator';
 
@@ -17,20 +21,33 @@ type Story = StoryObj<typeof DefaultErrorAlertFullScreen>;
 
 export const Default: Story = {
   name: '기본',
+  args: {
+    isShowingErrorIcon: true,
+    title: DEFAULT_ERROR_TITLE,
+    message: DEFAULT_ERROR_MESSAGE,
+    buttonIconId: 'refresh-line',
+    buttonText: '새로 고침하기',
+  },
 };
 
 export const NoIcon: Story = {
   name: '아이콘 제외',
   args: {
     isShowingErrorIcon: false,
+    title: DEFAULT_ERROR_TITLE,
+    message: DEFAULT_ERROR_MESSAGE,
+    buttonIconId: 'refresh-line',
+    buttonText: '새로 고침하기',
   },
 };
 
 export const CustomTexts: Story = {
   name: '텍스트 커스터마이징',
   args: {
+    isShowingErrorIcon: true,
     title: '커스텀 제목',
     message: '커스텀 설명',
+    buttonIconId: 'bar-chart-line',
     buttonText: '커스텀 버튼 제목',
   },
 };
