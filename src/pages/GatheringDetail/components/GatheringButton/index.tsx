fca6ca51d83d0da5fedb4dd0f589861d8997a977
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-15 01:59:50 +0900

    feat: 버튼별 기능 구현

--- src/pages/GatheringDetail/components/GatheringButton/index.tsx
+++ src/pages/GatheringDetail/components/GatheringButton/index.tsx
@@ -1,17 +1,24 @@
 import { useState } from 'react';
 
+import { useQueryClient } from '@tanstack/react-query';
 import { Link, useParams } from 'react-router-dom';
 
 import Button from '@/components/Button';
 import Modal from '@/components/Modal';
 import {
-  DELETE_TIP_MODAL_MESSAGE,
+  OUT_GATHERING_MODAL_MESSAGE,
   SUCCESS_GATHERING_JOIN_MODAL_MESSAGE,
+  SUCCESS_OUT_GATHERING_MODAL_MESSAGE,
 } from '@/constants/messages/modal';
 import {
   GATHERINGS_FIX_PAGE_URL,
   GATHERINGS_UNFIX_PAGE_URL,
 } from '@/constants/pageRoutes';
+import { GATHERING_DETAIL_QUERY_KEY } from '@/constants/queryKey';
+
+import { useGathering } from '../../hooks/useGatheringDelete';
+import { useGatheringNonparticipants } from '../../hooks/useGatheringNonparticipants';
+import { useGatheringParticipants } from '../../hooks/useGatheringParticipants';
 
 interface GatheringButtonProps {
   isParticipation: boolean;
@@ -19,8 +26,10 @@ interface GatheringButtonProps {
   isFix: '확정' | '미확정';
 }
 
-const NotParticipationButton = () => {
+const NotParticipationButton = ({ gatheringId }: { gatheringId: string }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const gatheringParticipants = useGatheringParticipants();
+  const queryClient = useQueryClient();
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -28,7 +37,13 @@ const NotParticipationButton = () => {
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
-    //api 호출코드 필요
+    queryClient.invalidateQueries({
+      queryKey: [GATHERING_DETAIL_QUERY_KEY, gatheringId],
+    });
+  };
+
+  const handleGatheringParticipants = () => {
+    gatheringParticipants(gatheringId, handleOpenModal);
   };
 
   return (
@@ -42,92 +57,138 @@ const NotParticipationButton = () => {
       >
         {SUCCESS_GATHERING_JOIN_MODAL_MESSAGE}
       </Modal>
-      <Button variant='primary' onClick={handleOpenModal}>
+      <Button variant='primary' onClick={handleGatheringParticipants}>
         {'입장하기'}
       </Button>
     </>
   );
 };
 
-const FixedGatheringButton = ({ boardGameId }: { boardGameId: string }) => {
+const FixedGatheringButton = ({ gatheringId }: { gatheringId: string }) => {
   return (
-    <Link to={`${GATHERINGS_UNFIX_PAGE_URL}/${boardGameId}`}>
+    <Link to={`${GATHERINGS_UNFIX_PAGE_URL}/${gatheringId}`}>
       <Button variant='danger'>모임 확정 취소하기</Button>
     </Link>
   );
 };
 
 const UnFixedIsLeaderGatheringButton = ({
-  boardGameId,
+  gatheringId,
 }: {
-  boardGameId: string;
+  gatheringId: string;
 }) => {
+  const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const gatheringDelete = useGathering();
 
-  const handleOpenModal = () => {
+  const handleOpenErrorModal = () => {
+    setIsErrorModalOpen(true);
+  };
+
+  const handleCloseErrorModal = () => {
+    setIsErrorModalOpen(false);
+  };
+
+  const handleClosePrimaryModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleOpenPrimaryModal = () => {
     setIsModalOpen(true);
   };
 
-  const handleDeleteModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-    //api 호출코드 필요
+  const handleDeleteErrorModal = () => {
+    setIsErrorModalOpen(false);
+    gatheringDelete(gatheringId, handleOpenPrimaryModal);
   };
 
   return (
     <>
       <Modal
         variant='danger'
-        isOpen={isModalOpen}
-        onClose={handleDeleteModal}
-        onDelete={handleCloseModal}
+        isOpen={isErrorModalOpen}
+        onClose={handleCloseErrorModal}
+        onDelete={handleDeleteErrorModal}
         title='안내'
         buttonChildren='확인'
       >
-        {DELETE_TIP_MODAL_MESSAGE}
+        {OUT_GATHERING_MODAL_MESSAGE}
       </Modal>
-      <Link to={`${GATHERINGS_FIX_PAGE_URL}/${boardGameId}`}>
+      <Modal
+        variant='primary'
+        isOpen={isModalOpen}
+        onClose={handleClosePrimaryModal}
+        title='안내'
+        buttonChildren='확인'
+      >
+        {SUCCESS_OUT_GATHERING_MODAL_MESSAGE}
+      </Modal>
+      <Link to={`${GATHERINGS_FIX_PAGE_URL}/${gatheringId}`}>
         <Button variant='primary'>모임 확정하기</Button>
       </Link>
-      <Button variant='danger' onClick={handleOpenModal}>
+      <Button variant='danger' onClick={handleOpenErrorModal}>
         모임 삭제하기
       </Button>
     </>
   );
 };
 
-const UnFixedIsParticipantGatheringButton = () => {
+const UnFixedIsParticipantGatheringButton = ({
+  gatheringId,
+}: {
+  gatheringId: string;
+}) => {
+  const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const gatheringNonparticipants = useGatheringNonparticipants();
+  const queryClient = useQueryClient();
 
-  const handleOpenModal = () => {
+  const handleOpenErrorModal = () => {
+    setIsErrorModalOpen(true);
+  };
+
+  const handleCloseErrorModal = () => {
+    setIsErrorModalOpen(false);
+  };
+
+  const handleClosePrimaryModal = () => {
+    setIsModalOpen(false);
+    queryClient.invalidateQueries({
+      queryKey: [GATHERING_DETAIL_QUERY_KEY, gatheringId],
+    });
+  };
+
+  const handleOpenPrimaryModal = () => {
     setIsModalOpen(true);
   };
 
-  const handleDeleteModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-    //api 호출코드 필요
+  const handleDeleteErrorModal = () => {
+    setIsErrorModalOpen(false);
+    gatheringNonparticipants(gatheringId, handleOpenPrimaryModal);
   };
 
   return (
     <>
       <Modal
         variant='danger'
-        isOpen={isModalOpen}
-        onClose={handleDeleteModal}
-        onDelete={handleCloseModal}
+        isOpen={isErrorModalOpen}
+        onClose={handleCloseErrorModal}
+        onDelete={handleDeleteErrorModal}
         title='안내'
         buttonChildren='확인'
       >
-        {DELETE_TIP_MODAL_MESSAGE}
+        {OUT_GATHERING_MODAL_MESSAGE}
       </Modal>
-      <Button variant='danger' onClick={handleOpenModal}>
+      <Modal
+        variant='primary'
+        isOpen={isModalOpen}
+        onClose={handleClosePrimaryModal}
+        title='안내'
+        buttonChildren='확인'
+      >
+        {SUCCESS_OUT_GATHERING_MODAL_MESSAGE}
+      </Modal>
+      <Button variant='danger' onClick={handleOpenErrorModal}>
         모임 나가기
       </Button>
     </>
@@ -139,18 +200,18 @@ const GatheringButton = ({
   isLeader,
   isFix,
 }: GatheringButtonProps) => {
-  const { boardGameId } = useParams() as { boardGameId: string };
+  const { gatheringId } = useParams() as { gatheringId: string };
 
   return (
     <div className='flex flex-col gap-4 p-4'>
       {!isParticipation ? (
-        <NotParticipationButton />
+        <NotParticipationButton gatheringId={gatheringId} />
       ) : isFix === '확정' ? (
-        <FixedGatheringButton boardGameId={boardGameId} />
+        <FixedGatheringButton gatheringId={gatheringId} />
       ) : isLeader ? (
-        <UnFixedIsLeaderGatheringButton boardGameId={boardGameId} />
+        <UnFixedIsLeaderGatheringButton gatheringId={gatheringId} />
       ) : (
-        <UnFixedIsParticipantGatheringButton />
+        <UnFixedIsParticipantGatheringButton gatheringId={gatheringId} />
       )}
     </div>
   );
