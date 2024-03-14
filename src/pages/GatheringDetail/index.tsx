f5ed51d700bcb0af6101b0dbcd715435cb80b554
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-15 00:44:51 +0900

    feat: 모임 상세 페이지 구현

--- src/pages/GatheringDetail/index.tsx
+++ src/pages/GatheringDetail/index.tsx
@@ -1,13 +1,57 @@
+import { useState } from 'react';
+
+import { useParams } from 'react-router-dom';
+
+import { useGetGatheringDetailApi } from '@/apis/gatheringDetail';
+import { useGetLoggedInUserApi } from '@/apis/getLoggedInUser';
 import TabBar from '@/components/TabBar';
 
+import GatheringButton from './components/GatheringButton';
+import GatheringIntroduce from './components/GatheringIntroduce';
+import GatheringParticipants from './components/GatheringParticipants';
+import TabMenu from './components/TabMenu';
+
 const GatheringDetailPage = () => {
+  const [activeTab, setActiveTab] = useState(0);
+  const { gatheringId } = useParams() as { gatheringId: string };
+  const gatheringDetail = useGetGatheringDetailApi(gatheringId);
+  const { participantResponse, isLeader, isFix, ...gatheringIntroduce } =
+    gatheringDetail;
+  const { id } = useGetLoggedInUserApi();
+
+  const handleTabSelect = (index: number) => {
+    setActiveTab(index);
+  };
+
+  const isParticipation = participantResponse.some(
+    ({ userId }) => userId === id,
+  );
+
   return (
-    <TabBar.Container>
-      <TabBar.Left>
-        <TabBar.GoBackButton />
-        <TabBar.Title>{}</TabBar.Title>
-      </TabBar.Left>
-    </TabBar.Container>
+    <div className='flex h-full flex-col'>
+      <TabBar.Container>
+        <TabBar.Left>
+          <TabBar.GoBackButton />
+        </TabBar.Left>
+        <TabBar.Right>
+          <TabBar.ShareButton />
+        </TabBar.Right>
+      </TabBar.Container>
+      <TabMenu
+        tabs={['모임 소개', `참가자 (${participantResponse.length})`]}
+        onSelectTab={handleTabSelect}
+      />
+      {activeTab ? (
+        <GatheringParticipants participants={participantResponse} />
+      ) : (
+        <GatheringIntroduce gatheringIntroduce={gatheringIntroduce} />
+      )}
+      <GatheringButton
+        isLeader={isLeader}
+        isParticipation={isParticipation}
+        isFix={isFix}
+      />
+    </div>
   );
 };
 
