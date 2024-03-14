5a3859678570239d44b97ada751c962095835186
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-15 00:43:08 +0900

    refactor: 모임상세 상대시간 반영 및 줄바꿈 스타일 변경

--- src/pages/GatheringDetail/components/GatheringDescription/GatheringDescription.tsx
+++ src/pages/GatheringDetail/components/GatheringDescription/GatheringDescription.tsx
@@ -1,19 +1,25 @@
+import { getRelativeTimeWithin } from '@/utils/time';
+
 interface GatheringDescriptionProps {
   title: string;
   description: string;
+  createdAt: string;
 }
 
 const GatheringDescription = ({
   title,
+  createdAt,
   description,
 }: GatheringDescriptionProps) => {
   return (
-    <div className='flex w-full flex-col'>
+    <div className='flex flex-col'>
       <span className='text-xl font-bold'>{title}</span>
       <span className='mb-4 mt-1 text-[10px] text-gray-accent3 underline'>
-        3시간 전에 작성됨
+        {getRelativeTimeWithin(createdAt)}
       </span>
-      <p className='text-sm text-gray-accent2'>{description}</p>
+      <p className='whitespace-normal text-sm text-gray-accent2'>
+        {description}
+      </p>
     </div>
   );
 };
