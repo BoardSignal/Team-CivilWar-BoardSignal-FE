1fa9e67b508720e93414caa04e68b73e9fc9e680
Heeeera <happynet.choi@gmail.com>
2024-03-16 13:41:19 +0900

    feat: storybook 파일 추가

--- src/pages/EndGames/components/EndGameList/index.tsx
+++ src/pages/EndGames/components/EndGameList/index.tsx
@@ -8,6 +8,8 @@ import SpinnerListBottom from '@/components/Spinner/SpinnerListBottom';
 import { END_GAMES_REVIEWS_CREATE_PAGE_URL } from '@/constants/pageRoutes';
 import { getRemainDay } from '@/utils/time';
 
+const REVIEW_PERIOD = 5;
+
 interface EndGameListProps {
   userId: number;
 }
@@ -22,6 +24,7 @@ const EndGameList = ({ userId }: EndGameListProps) => {
       fetchNextPage={fetchNextPage}
       fetchStatus={isFetchingNextPage ? 'fetching' : 'idle'}
       loaderElement={<SpinnerListBottom />}
+      className='grow overflow-y-auto'
     >
       <ul>
         {endGames.map(endGame => (
@@ -35,12 +38,12 @@ const EndGameList = ({ userId }: EndGameListProps) => {
               isFullDate
               className='border-b-0 p-0'
             />
-            {getRemainDay(endGame.createdAt) > 0 && (
+            {getRemainDay(endGame.createdAt) < REVIEW_PERIOD && (
               <Link to={END_GAMES_REVIEWS_CREATE_PAGE_URL}>
                 <Button
                   variant='primary'
                   className=''
-                >{`모임 리뷰하기 (${getRemainDay(endGame.createdAt)}일 남음)`}</Button>
+                >{`모임 리뷰하기 (${REVIEW_PERIOD - getRemainDay(endGame.createdAt)}일 남음)`}</Button>
               </Link>
             )}
           </li>
