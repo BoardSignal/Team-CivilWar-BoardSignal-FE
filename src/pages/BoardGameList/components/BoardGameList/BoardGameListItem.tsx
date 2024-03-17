2b00ddaa24b53e4ae0a78a3498aebbbfc86a6623
Seongbin Kim <seongbin9786@gmail.com>
2024-03-17 11:51:42 +0900

    fix: 누락된 요구사항 추가 구현

--- src/pages/BoardGameList/components/BoardGameList/BoardGameListItem.tsx
+++ src/pages/BoardGameList/components/BoardGameList/BoardGameListItem.tsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 import { BoardGameListItemResponseDTO } from '@/apis/boardGameList';
 import Button from '@/components/Button';
 import Chip from '@/components/Chip';
 import Icon from '@/components/Icon';
+import { BOARD_GAMES_PAGE_URL } from '@/constants/pageRoutes';
 
 type BoardGameListItemProps = {
   boardGame: BoardGameListItemResponseDTO;
@@ -23,6 +26,7 @@ const DEFAULT_BOARD_GAME_IMAGE_URL = 'https://picsum.photos/200/300';
 
 const BoardGameListItem = ({
   boardGame: {
+    boardGameId,
     name,
     categories,
     difficulty,
@@ -34,39 +38,48 @@ const BoardGameListItem = ({
     imageUrl,
   },
 }: BoardGameListItemProps) => {
+  const participantsText =
+    minParticipants === maxParticipants
+      ? minParticipants.toString()
+      : [minParticipants, maxParticipants].join('-');
+
+  const playTimeText =
+    fromPlayTime === toPlayTime
+      ? fromPlayTime.toString()
+      : `${[fromPlayTime, toPlayTime].join('-')}분`;
+
   return (
-    <Button className='flex h-fit justify-start gap-4 rounded-none p-4'>
-      <img
-        src={imageUrl ?? DEFAULT_BOARD_GAME_IMAGE_URL}
-        className='size-[100px] rounded-lg border border-gray-accent7 object-contain'
-      />
-      <div className='flex flex-col items-start gap-2'>
-        <div className='flex flex-col gap-1'>
-          <span className='font-bold text-gray-accent1'>{name}</span>
+    <Link to={`${BOARD_GAMES_PAGE_URL}/${boardGameId}`}>
+      <Button className='flex h-fit justify-start gap-4 rounded-none border-b border-gray-accent7 p-4'>
+        <img
+          src={imageUrl ?? DEFAULT_BOARD_GAME_IMAGE_URL}
+          className='size-[100px] rounded-lg border border-gray-accent7 object-contain'
+        />
+        <div className='flex flex-col items-start gap-2'>
+          <div className='flex flex-col gap-1'>
+            <span className='font-bold text-gray-accent1'>{name}</span>
+            <div className='flex gap-1'>
+              {/* 카테고리가 여러 개 인 경우의 디자인이 없어서 gap: 4px로 지정했어요 */}
+              {categories.map(category => (
+                <Chip key={category}>{category}</Chip>
+              ))}
+            </div>
+          </div>
           <div className='flex gap-1'>
-            {/* 카테고리가 여러 개 인 경우의 디자인이 없어서 gap: 4px로 지정했어요 */}
-            {categories.map(category => (
-              <Chip key={category}>{category}</Chip>
-            ))}
+            <Description title='난이도' value={difficulty} />
+            <Description title='인원' value={participantsText} />
+            <Description title='소요시간' value={playTimeText} />
+          </div>
+          <div className='flex items-center gap-2 py-0.5'>
+            <Icon
+              id='bookmark-line'
+              className='size-[14px] text-gray-accent1'
+            />
+            <span className='text-xs text-gray-accent2'>{wishCount}</span>
           </div>
         </div>
-        <div className='flex gap-1'>
-          <Description title='난이도' value={difficulty} />
-          <Description
-            title='인원'
-            value={[minParticipants, maxParticipants].join('-')}
-          />
-          <Description
-            title='소요시간'
-            value={`${[fromPlayTime, toPlayTime].join('-')}분`}
-          />
-        </div>
-        <div className='flex gap-2 py-0.5'>
-          <Icon id='bookmark-line' className='size-[14px] text-gray-accent1' />
-          <span className='text-xs text-gray-accent2'>{wishCount}</span>
-        </div>
-      </div>
-    </Button>
+      </Button>
+    </Link>
   );
 };
 
