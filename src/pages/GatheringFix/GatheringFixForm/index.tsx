54e03e712c37cf733949a5569dca887d1c1c7fc2
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-03-19 19:38:44 +0900

    refactor: 상수화 진행

--- src/pages/GatheringFix/GatheringFixForm/index.tsx
+++ src/pages/GatheringFix/GatheringFixForm/index.tsx
@@ -8,6 +8,10 @@ import Label from '@/components/Label';
 import LocationSelect from '@/components/LocationSelect';
 import Select from '@/components/Select';
 import SubwaySelect from '@/components/SubwaySelect';
+import {
+  GATHERING_FIX_ALERT_MESSAGE,
+  GATHERING_FIX_DATE_ALERT_MESSAGE,
+} from '@/constants/messages/alert';
 
 import { GatheringFixFormValues, gatheringFixFormOptions } from './formSchema';
 import gatheringFixRequestMapper from './gatheringFixRequestMapper';
@@ -72,7 +76,7 @@ const GatheringFixForm = ({ onCreate }: GatheringFixFormProps) => {
       <section className='flex flex-col gap-4 p-4'>
         <div className='flex h-full grow flex-col gap-8 px-4'>
           <div className='-mb-4'>
-            <Alert>모임이 확정된 시작 시각과 장소를 입력해주세요</Alert>
+            <Alert>{GATHERING_FIX_ALERT_MESSAGE}</Alert>
           </div>
           <Label isRequired title='시작 시간'>
             <Select
@@ -92,10 +96,10 @@ const GatheringFixForm = ({ onCreate }: GatheringFixFormProps) => {
                 <>
                   <Label title='모임 날짜' isRequired>
                     <DatePicker value={value} onChange={onChange} />
+                    <Alert variant='danger'>
+                      {GATHERING_FIX_DATE_ALERT_MESSAGE}
+                    </Alert>
                   </Label>
-                  <Alert variant='danger'>
-                    날짜는 현재 시간 기준 7일 이내로 선택해야 합니다.
-                  </Alert>
                 </>
               );
             }}
