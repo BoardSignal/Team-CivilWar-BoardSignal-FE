f1e57ceabc9cef8ace45cb055b5a5074aa0b1834
Heeeera <happynet.choi@gmail.com>
2024-03-20 13:40:45 +0900

    fix: TabBar에 프로필 이미지 추가

--- src/pages/EndGames/index.tsx
+++ src/pages/EndGames/index.tsx
@@ -1,17 +1,27 @@
 import { useGetLoggedInUserApi } from '@/apis/loggedInUser';
+import DefaultProfileImage from '@/assets/default-profile-image.png';
 import TabBar from '@/components/TabBar';
 
 import EndGameList from './components/EndGameList';
 
 const EndGamesPage = () => {
-  const { nickname, id } = useGetLoggedInUserApi();
+  const { nickname, profileImageUrl, id } = useGetLoggedInUserApi();
 
   return (
     <div className='flex h-full flex-col'>
       <TabBar.Container>
         <TabBar.Left>
           <TabBar.GoBackButton />
-          <TabBar.Title>{nickname}</TabBar.Title>
+          <TabBar.Title>
+            <div className='flex gap-1'>
+              <span>{nickname}</span>
+              <img
+                src={profileImageUrl ?? DefaultProfileImage}
+                alt='프로필'
+                className='size-6'
+              />
+            </div>
+          </TabBar.Title>
         </TabBar.Left>
       </TabBar.Container>
       <EndGameList userId={id} />
